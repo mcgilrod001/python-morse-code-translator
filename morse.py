f2b6a9195e16e7bdeddd894f6e243105442b02c7
Roddy <roddymcg787@gmail.com>
2023-11-09 13:24:14 -0800

    added a ui

--- morse.py
+++ morse.py
@@ -1,91 +1,93 @@
 import alphebet
+def translate(plain_text):
+    # plain_text = input("enter you message: ")
+    decoded_text = []
+    #turns plain text into list
+    parse_text = list(plain_text)
+    # print(parse_text)
+    for i in range(len(parse_text)):
+        if parse_text[i] == "a":
+            # appends the morse code for a to the list decoded_text
+            decoded_text.append(alphebet.a)
+            decoded_text.append(" ")
+        elif parse_text[i] == "b":
+            decoded_text.append(alphebet.b)
+            decoded_text.append(" ")
+        elif parse_text[i] == "c":
+            decoded_text.append(alphebet.c)
+            decoded_text.append(" ")
+        elif parse_text[i] == "d":
+            decoded_text.append(alphebet.d)
+            decoded_text.append(" ")
+        elif parse_text[i] == "e":
+            decoded_text.append(alphebet.e)
+            decoded_text.append(" ")
+        elif parse_text[i] == "f":
+            decoded_text.append(alphebet.f)
+            decoded_text.append(" ")
+        elif parse_text[i] == "g":
+            decoded_text.append(alphebet.g)
+            decoded_text.append(" ")
+        elif parse_text[i] == "h":
+            decoded_text.append(alphebet.h)
+            decoded_text.append(" ")
+        elif parse_text[i] == "i":
+            decoded_text.append(alphebet.i)
+            decoded_text.append(" ")
+        elif parse_text[i] == "j":
+            decoded_text.append(alphebet.j)
+            decoded_text.append(" ")
+        elif parse_text[i] == "k":
+            decoded_text.append(alphebet.k)
+            decoded_text.append(" ")
+        elif parse_text[i] == "l":
+            decoded_text.append(alphebet.l)
+            decoded_text.append(" ")
+        elif parse_text[i] == "m":
+            decoded_text.append(alphebet.m)
+            decoded_text.append(" ")
+        elif parse_text[i] == "n":
+            decoded_text.append(alphebet.n)
+            decoded_text.append(" ")
+        elif parse_text[i] == "o":
+            decoded_text.append(alphebet.o)
+            decoded_text.append(" ")
+        elif parse_text[i] == "p":
+            decoded_text.append(alphebet.p)
+            decoded_text.append(" ")
+        elif parse_text[i] == "q":
+            decoded_text.append(alphebet.q)
+            decoded_text.append(" ")
+        elif parse_text[i] == "r":
+            decoded_text.append(alphebet.r)
+            decoded_text.append(" ")
+        elif parse_text[i] == "s":
+            decoded_text.append(alphebet.s)
+            decoded_text.append(" ")
+        elif parse_text[i] == "t":
+            decoded_text.append(alphebet.t)
+            decoded_text.append(" ")
+        elif parse_text[i] == "u":
+            decoded_text.append(alphebet.u)
+            decoded_text.append(" ")
+        elif parse_text[i] == "v":
+            decoded_text.append(alphebet.v)
+            decoded_text.append(" ")
+        elif parse_text[i] == "w":
+            decoded_text.append(alphebet.w)
+            decoded_text.append(" ")
+        elif parse_text[i] == "x":
+            decoded_text.append(alphebet.x)
+            decoded_text.append(" ")
+        elif parse_text[i] == "y":
+            decoded_text.append(alphebet.y)
+            decoded_text.append(" ")
+        elif parse_text[i] == "z":
+            decoded_text.append(alphebet.z)
+            decoded_text.append(" ")
+        else:
+            decoded_text.append("   ")
+    decoded_text = "".join(decoded_text)
+    return(decoded_text)
 
-plain_text = input("enter you message: ")
-decoded_text = []
-#turns plain text into list
-parse_text = list(plain_text)
-print(parse_text)
-for i in range(len(parse_text)):
-    if parse_text[i] == "a":
-        # appends the morse code for a to the list decoded_text
-        decoded_text.append(alphebet.a)
-        decoded_text.append(" ")
-    elif parse_text[i] == "b":
-        decoded_text.append(alphebet.b)
-        decoded_text.append(" ")
-    elif parse_text[i] == "c":
-        decoded_text.append(alphebet.c)
-        decoded_text.append(" ")
-    elif parse_text[i] == "d":
-        decoded_text.append(alphebet.d)
-        decoded_text.append(" ")
-    elif parse_text[i] == "e":
-        decoded_text.append(alphebet.e)
-        decoded_text.append(" ")
-    elif parse_text[i] == "f":
-        decoded_text.append(alphebet.f)
-        decoded_text.append(" ")
-    elif parse_text[i] == "g":
-        decoded_text.append(alphebet.g)
-        decoded_text.append(" ")
-    elif parse_text[i] == "h":
-        decoded_text.append(alphebet.h)
-        decoded_text.append(" ")
-    elif parse_text[i] == "i":
-        decoded_text.append(alphebet.i)
-        decoded_text.append(" ")
-    elif parse_text[i] == "j":
-        decoded_text.append(alphebet.j)
-        decoded_text.append(" ")
-    elif parse_text[i] == "k":
-        decoded_text.append(alphebet.k)
-        decoded_text.append(" ")
-    elif parse_text[i] == "l":
-        decoded_text.append(alphebet.l)
-        decoded_text.append(" ")
-    elif parse_text[i] == "m":
-        decoded_text.append(alphebet.m)
-        decoded_text.append(" ")
-    elif parse_text[i] == "n":
-        decoded_text.append(alphebet.n)
-        decoded_text.append(" ")
-    elif parse_text[i] == "o":
-        decoded_text.append(alphebet.o)
-        decoded_text.append(" ")
-    elif parse_text[i] == "p":
-        decoded_text.append(alphebet.p)
-        decoded_text.append(" ")
-    elif parse_text[i] == "q":
-        decoded_text.append(alphebet.q)
-        decoded_text.append(" ")
-    elif parse_text[i] == "r":
-        decoded_text.append(alphebet.r)
-        decoded_text.append(" ")
-    elif parse_text[i] == "s":
-        decoded_text.append(alphebet.s)
-        decoded_text.append(" ")
-    elif parse_text[i] == "t":
-        decoded_text.append(alphebet.t)
-        decoded_text.append(" ")
-    elif parse_text[i] == "u":
-        decoded_text.append(alphebet.u)
-        decoded_text.append(" ")
-    elif parse_text[i] == "v":
-        decoded_text.append(alphebet.v)
-        decoded_text.append(" ")
-    elif parse_text[i] == "w":
-        decoded_text.append(alphebet.w)
-        decoded_text.append(" ")
-    elif parse_text[i] == "x":
-        decoded_text.append(alphebet.x)
-        decoded_text.append(" ")
-    elif parse_text[i] == "y":
-        decoded_text.append(alphebet.y)
-        decoded_text.append(" ")
-    elif parse_text[i] == "z":
-        decoded_text.append(alphebet.z)
-        decoded_text.append(" ")
-    else:
-        decoded_text.append("   ")
-decoded_text = "".join(decoded_text)
-print(decoded_text)
+# print(translate("hello world"))
---
+++ ui.py
@@ -0,0 +1,35 @@
+import customtkinter as ct# import playsound
+import morse
+
+# sets default theme to dark blue
+ct.set_default_color_theme('dark-blue')
+ct.set_appearance_mode('dark')
+root = ct.CTk()
+root.title('morse code translator')
+root.geometry('500x250')
+
+# creates frame 
+frame = ct.CTkFrame(master=root)
+frame.pack(pady=20  , padx=50, fill="both", expand=True)
+
+# creates label
+label = ct.CTkLabel(master=frame, text="morse code translator", font=("roboto", 20))
+label.pack(pady=12,padx=10)
+
+# creates entry
+entry1 = ct.CTkEntry(master=frame, placeholder_text="enter text here", font=("roboto", 20), width=240,height=60)
+entry1.pack(pady=12,padx=10, expand=True, fill="both")
+
+# creates text box 
+# ctk_text = ct.CTkTextbox(master=frame, font=("roboto", 20)).pack(expand=True, fill="both")
+# label2 = ct.CTkLabel(master=frame, text="translated text", font=("roboto", 20))
+# label2.pack(pady=12,padx=10)
+def get_inf_from_text():
+    print(morse.translate(plain_text=entry1.get()))
+# creats button
+button = ct.CTkButton(master=frame, text="translate", font=("roboto", 20),command=get_inf_from_text).pack(pady=12,padx=10)
+
+
+
+
+root.mainloop()
